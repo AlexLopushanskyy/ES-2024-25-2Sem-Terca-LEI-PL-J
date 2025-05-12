fd63f5ba9e9aea870f76bfe456fa9e9b6675d3da
AlexLopushanskyy <aggly@iscte-iul.pt>
2025-05-12 15:45:10 +0100

    classe GrafoProprietarios

---
+++ src/main/java/GrafoProprietarios.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class GrafoProprietarios {
+
+    private Map<Integer, Set<Integer>> grafo = new HashMap<>(); // OwnerID -> Set de OwnerIDs vizinhos
+}
