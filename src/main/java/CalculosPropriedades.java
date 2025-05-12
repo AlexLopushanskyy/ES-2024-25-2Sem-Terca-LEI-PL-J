c460d0643ab6efcac7292a0e57c84aa5b4980e25
JoaoLouro <jmmlo1@iscte-iul.pt>
2025-05-12 18:50:09 +0100

    construtor

--- src/main/java/CalculosPropriedades.java
+++ src/main/java/CalculosPropriedades.java
@@ -3,7 +3,14 @@ import java.util.List;
 
 public class CalculosPropriedades {
 
+    private List<Propriedade> propriedades;
 
-    
+    public CalculosPropriedades(List<Propriedade> propriedades) {
+        this.propriedades = propriedades;
+    }
+
+  
+
+
 }
 
